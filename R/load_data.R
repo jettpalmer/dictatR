7fe77a64bfc62f72e50f3d442bd14f7f43cf4b5d
Jett Palmer <jettdpalmer@gmail.com>
2026-05-23 14:57:37 -0700

    all dependencies accounted for

--- R/load_data.R
+++ R/load_data.R
@@ -1,4 +1,17 @@
-load_data <- function(){
-  readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024-11-05/democracy_data.csv')
-
+#' Load democracy and dictatorship data
+#'
+#' Downloads the democracy dataset from the TidyTuesday GitHub repository
+#' and returns it as a tibble.
+#'
+#' @return A tibble containing democracy and dictatorship data by country and year.
+#' @importFrom readr read_csv
+#' @export
+#'
+#' @examples
+#' democracy_data <- load_data()
+#' head(democracy_data)
+load_data <- function() {
+  readr::read_csv(
+    "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024-11-05/democracy_data.csv"
+  )
 }

6aad7e73589ab686f81d18067185c9bb2cc31262
Jett Palmer <jettdpalmer@gmail.com>
2026-05-30 14:44:07 -0700

    first attempt at make_regime_map test

--- tests/testthat/test-make_regime_map.R
+++ tests/testthat/test-make_regime_map.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("make_regime_map works", {
+  democracy_data <- load_data()
+  world_data <- load_world_data()
+
+  regime_map <- make_regime_map(democracy_data, world_data)
+
+  expect_s3_class(regime_map, "leaflet")
+  expect_s3_class(regime_map, "htmlwidget")
 })

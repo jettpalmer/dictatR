6b5820255260af6ee4b7d7b9ee75813ddc5ebf0f
Jett Palmer <jettdpalmer@gmail.com>
2026-06-03 12:45:49 -0700

    example and context for coup data function

--- R/load_coup_data.R
+++ R/load_coup_data.R
@@ -1,10 +1,15 @@
 #' Load coup data
 #'
-#' Downloads the coup dataset and returns it as a tibble.
+#' Downloads the coup dataset from Powell & Thyne (2011)
+#' and returns it as a tibble.
 #'
 #' @return A tibble containing coup attempts and outcomes by country and year.
 #' @importFrom readr read_tsv
 #' @export
+#'
+#' #' @examples
+#' coups <- load_coup_data()
+#' head(coups)
 load_coup_data <- function() {
   readr::read_tsv(
     "http://www.uky.edu/~clthyn2/coup_data/candidate_dataset.txt"

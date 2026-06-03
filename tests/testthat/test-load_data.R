467b80d1526e06102faf4973595515824599cc8c
Jett Palmer <jettdpalmer@gmail.com>
2026-06-03 12:42:55 -0700

    added test for load data

---
+++ tests/testthat/test-load_data.R
@@ -0,0 +1,7 @@
+test_that("load_data works", {
+  democracy_data <- load_data()
+
+  expect_s3_class(democracy_data, "tbl_df")
+  expect_gt(nrow(democracy_data), 0)
+  expect_true("country_name" %in% names(democracy_data))
+})

712ffff994a976b8a14529391a818ed06e85982f
Jett Palmer <jettdpalmer@gmail.com>
2026-06-03 12:43:26 -0700

    added test for load_world_data

---
+++ tests/testthat/test-load_world_data.R
@@ -0,0 +1,7 @@
+test_that("load_world_data works", {
+  world_data <- load_world_data()
+
+  expect_s3_class(world_data, "sf")
+  expect_gt(nrow(world_data), 0)
+  expect_true("geometry" %in% names(world_data))
+})

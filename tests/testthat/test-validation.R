44c3c49d0cc8f0e217df646478ec0e7ab0befb14
Jett Palmer <jettdpalmer@gmail.com>
2026-06-06 11:27:15 -0700

    validate_region test

--- tests/testthat/test-validation.R
+++ tests/testthat/test-validation.R
@@ -2,7 +2,8 @@ test_that("validate_dataset works", {
   data <- load_data()
 
   expect_invisible(validate_dataset(data))
-  expect_error(validate_dataset("not a dataframe"), "data input must be a dataframe")
+  expect_error(validate_dataset("not a dataframe"),
+               "data input must be a dataframe")
 })
 
 test_that("validate_country works", {
@@ -10,15 +11,24 @@ test_that("validate_country works", {
 
   expect_invisible(validate_country("BOL", data))
 
-  expect_error(
-    validate_country(123, dat),
-    "Country code input must be a string")
+  expect_error(validate_country(123, dat),
+               "Country code input must be a string")
 
-  expect_error(
-    validate_country("BO", dat),
-    "Country code input must three letters")
+  expect_error(validate_country("BO", dat),
+               "Country code input must three letters")
 
-  expect_error(
-    validate_country("ZZZ", dat),
-    "Country code not found in the dataset")
+  expect_error(validate_country("ZZZ", dat),
+               "Country code not found in the dataset")
+})
+
+test_that("validate_region works", {
+  expect_invisible(validate_region("South Asia"))
+
+  expect_error(validate_region(123),
+               "Region input must be a string")
+
+  expect_error(validate_region("Jett's house"),
+               "Region must be one of: East Asia & Pacific, Europe & Central Asia,
+    Latin America & Caribbean, Middle East & North Africa, North America,
+    South Asia, Sub-Saharan Africa.")
 })

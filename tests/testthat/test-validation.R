ff2c7d900f0acf699ce64c47d14a89e67f8bbca9
Jett Palmer <jettdpalmer@gmail.com>
2026-06-06 11:25:09 -0700

    validate_country tests

--- tests/testthat/test-validation.R
+++ tests/testthat/test-validation.R
@@ -5,4 +5,20 @@ test_that("validate_dataset works", {
   expect_error(validate_dataset("not a dataframe"), "data input must be a dataframe")
 })
 
+test_that("validate_country works", {
+  data <- load_data()
 
+  expect_invisible(validate_country("BOL", data))
+
+  expect_error(
+    validate_country(123, dat),
+    "Country code input must be a string")
+
+  expect_error(
+    validate_country("BO", dat),
+    "Country code input must three letters")
+
+  expect_error(
+    validate_country("ZZZ", dat),
+    "Country code not found in the dataset")
+})
